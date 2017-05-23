8d7587a70cbd7e31f18108b4e7b553cb0550fe91
giancarlovs2891 <giancarlo.valencia@saludtec.co>
2017-05-23 10:55:40 -0500

    main layout user sign in and sing out

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,5 +1,6 @@
 class CoursesController < ApplicationController
   before_action :set_course, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:index, :show, :new]
 
   # GET /courses
   # GET /courses.json
