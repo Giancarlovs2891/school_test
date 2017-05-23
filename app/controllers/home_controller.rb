199bd02ca95c744b6e99fe07e8567f8ef4dac0bd
giancarlovs2891 <giancarlo.valencia@saludtec.co>
2017-05-23 18:03:15 -0500

    changes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,7 @@ class HomeController < ApplicationController
 
     def index
       if user_signed_in?
-        redirect_to "home#show"
+        redirect_to "/main"
       end
     end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+user = User.new
+user.email = 'admin@test.nativapps.com'
+user.password = 'admin123'
+user.password_confirmation = 'admin123'
+user.save!
