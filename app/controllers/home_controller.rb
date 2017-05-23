53a1fa242a11a7b8fc25c289443d1daa634f9b77
giancarlovs2891 <giancarlo.valencia@saludtec.co>
2017-05-23 12:00:39 -0500

    navigation and user authentication validation added

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,12 @@
-class HomeController < ApplicationController 
+class HomeController < ApplicationController
+  before_action :authenticate_user!, only: [:show]
 
     def index
+      if user_signed_in?
+        redirect_to "home#show"
+      end
     end
 
+    def show
+    end
 end
